3f5a1b4e225ca96b45703833fbf9091f633d892c
DESKTOP-IE5CFA3\Francisco <sanchez.sabido@gmail.com>
2025-12-24 21:59:25 +0100

    Ajustes para tener el PWA

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -1,1 +1,12 @@
-self.addEventListener("fetch", () => {});
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
+
